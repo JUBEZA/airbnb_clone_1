067aa9af4020e36279e7c3f196aa6de3ea652da0
Juan Bezanilla <juanbezanillaz@gmail.com>
2024-05-17 13:24:45 -0400

    creando el resto de las Migraciones

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+
+  has_many :properties
+  has_many :bookings
+  has_many :reviews
 end
