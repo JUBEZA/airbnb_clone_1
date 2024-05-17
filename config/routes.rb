de2cd9c67e3e83884bee85e5a39ca1b6968d24f8
Juan Bezanilla <juanbezanillaz@gmail.com>
2024-05-17 14:09:58 -0400

    Rutas Modificadas

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :properties do
+    resources :bookings, only: [:create, :new]
+    resources :reviews, only: [:create, :new]
+  end
+
+  resources :bookings, only: [:index, :show, :update, :destroy]
+  resources :reviews, only: [:index, :show, :update, :destroy]
 end
