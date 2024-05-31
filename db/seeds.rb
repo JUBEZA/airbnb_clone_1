25d81b9e16cd7dceb78ed97b9dfbea077106b34f
Juan Bezanilla <juanbezanillaz@gmail.com>
2024-05-31 12:54:02 -0400

    API instalado

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create!(
+  email: 'test@example.com',
+  password: 'password',
+  password_confirmation: 'password',
+  name: 'Test User'
+)
+
+properties = [
+  { title: 'Cozy Cottage', description: 'A cozy cottage in the countryside', price: 100.0, location: 'Countryside', user_id: User.first.id },
+  { title: 'Modern Apartment', description: 'A modern apartment in the city center', price: 150.0, location: 'City Center', user_id: User.first.id },
+  { title: 'Beach House', description: 'A beautiful beach house with ocean view', price: 200.0, location: 'Beach', user_id: User.first.id }
+]
+
+properties.each do |property|
+  Property.create!(property)
+end
